d813803f7e8629f6a6ca854a066a142169dec765
JatinAbrol <Jatinabrol09@gmail.com>
2022-06-03 20:06:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chat-Box-Profanity
+
+Profanity Model
+
+https://colab.research.google.com/drive/1gVAlYOeDDh6rH4xBlbOdUjGSArgU6CqH?usp=sharing
---
+++ app/src/main/java/com/example/chatterbox/TabAccessorAdapter.java
@@ -0,0 +1,70 @@
+package com.example.chatterbox;
+
+//import android.support.v4.app.Fragment;
+//import android.support.annotation.Nullable;
+//import android.support.v4.app.Fragment;
+//import android.support.v4.app.FragmentManager;
+//import android.support.v4.app.FragmentPagerAdapter;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.chatterbox.fragments.ChatsFragment;
+import com.example.chatterbox.fragments.ContactsFragment;
+import com.example.chatterbox.fragments.GroupsFragment;
+import com.example.chatterbox.fragments.RequestsFragment;
+
+public class TabAccessorAdapter extends FragmentPagerAdapter {
+    public TabAccessorAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+            case 3:
+                RequestsFragment requestsFragment = new RequestsFragment();
+                return requestsFragment;
+
+            default:
+               return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Chats";
+            case 1:
+                return "Groups";
+            case 2:
+                return "Contacts";
+            case 3:
+                return "Requests";
+
+            default:
+                return null;
+        }
+
+//        return super.getPageTitle(position);
+    }
+}
